f7c15589ec2b7e263abb43668ab9bba832370b6b
joon0448@beejh02 <joon0448@naver.com>
2025-09-11 15:06:41 +0900

    feat : 틀잡아두기

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
-import { Link, Outlet } from 'react-router-dom';
+// Link 대신 NavLink를 import 합니다.
+import { NavLink, Outlet } from 'react-router-dom';
 import './Layout.css';
 
 const Layout = () => (
   <div className="layout">
     <header>
-      <nav>
-        <Link to="/" className="logo">
-          MySite
-        </Link>
+      <nav id="navbar">
+        <NavLink to="/" className="logo">
+          MyPortfolio
+        </NavLink>
         <div className="nav-links">
-          <Link to="/" className="nav-link">
-            Home
-          </Link>
-          <Link to="/about" className="nav-link">
-            About
-          </Link>
-          <Link to="/contact" className="nav-link">
+          <NavLink to="/projects" className="nav-link">
+            Projects
+          </NavLink>
+          <NavLink to="/skills" className="nav-link">
+            Skills
+          </NavLink>
+          <NavLink to="/experience" className="nav-link">
+            Experience
+          </NavLink>
+          <NavLink to="/contact" className="nav-link">
             Contact
-          </Link>
+          </NavLink>
         </div>
       </nav>
     </header>
+
     <main>
       <Outlet />
     </main>
+
     <footer>
-        <p>&copy; 2025 MySite. All rights reserved.</p>
+      <p>&copy; 2025 MyPortfolio. All rights reserved.</p>
     </footer>
   </div>
 );
